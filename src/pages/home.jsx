1fb870b6c14416b899f764eb6d309bf99f708ca3
Dan George <dangeorge35@gmail.com>
2024-08-01 07:18:41 +0100

    updated

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,31 @@
 import React, { Component } from "react";
 import Header from "../components/header";
 class Home extends Component {
+  handlemessage = (var1, var2) => {
+    alert("Hello, World!");
+    window.ReactNativeWebView.postMessage(
+      JSON.stringify({
+        title: var1,
+        content: var2,
+        notiType: "push_notification",
+        type: "success",
+      })
+    );
+  };
   render() {
     return (
       <div>
         <Header />
+        <div>
+          <button
+            className="btn btn-primary "
+            onClick={() => {
+              this.handlemessage("test", "testing");
+            }}
+          >
+            Notification
+          </button>
+        </div>
       </div>
     );
   }
