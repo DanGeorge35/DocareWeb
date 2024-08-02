4a15c7ee645188475357a5f00780aba1a44f7fed
Dan George <dangeorge35@gmail.com>
2024-08-02 06:14:06 +0100

    updated

--- src/services/auth.js
+++ src/services/auth.js
@@ -3,7 +3,7 @@ import { ENDPOINTS } from "./api/endpoints";
 
 export const login = async (credentials) => {
   try {
-    const response = await apiClient.post(ENDPOINTS.LOGIN, credentials);
+    const response = await server.post(ENDPOINTS.LOGIN, credentials);
     return response.data;
   } catch (error) {
     throw error;
