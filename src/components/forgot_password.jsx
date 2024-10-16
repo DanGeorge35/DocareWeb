d07acaf593b9ea96c5fce917232dbb1633c7f90e
Dan George <dangeorge35@gmail.com>
2024-10-16 08:46:39 +0100

    Update

--- src/components/forgot_password.jsx
+++ src/components/forgot_password.jsx
@@ -1,47 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const ForgotPassword = () => {
   const navigate = useNavigate();
 
+  // State for email input
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+
+  // Function to handle password reset
+  const handleReset = async () => {
+    try {
+      const response = await axios.post("YOUR_SERVER_URL/api/forgot-password", {
+        email,
+      });
+
+      // If the request is successful
+      if (response.status === 200) {
+        setSuccess("Password reset link has been sent to your email.");
+        setError("");
+      }
+    } catch (err) {
+      // Handle errors
+      setError("Unable to send password reset email. Please try again.");
+      setSuccess("");
+    }
+  };
+
   return (
-    <div className="screen  login_bg bg-info w3-display-container ">
-      <div className="w3-display-middle w-100 container">
-        <div className="row">
-          <div className="col-lg-6 offset-lg-3 text-center">
-            <img src="logo_white.png" alt="logo" style={{ height: "80px" }} />
-            <h3 className="text-center text-white w3-bold">
-              <b>DOCARE</b>
-            </h3>
-            <b className="text-center text-white  mb-4 pt-5 pb-4 w3-block h5 bold">
-              Forgot your password?
-            </b>
+    <div className="bg-info container0">
+      <div className="row">
+        <div className="col-xl-6 p-0 d-none d-lg-block">
+          <div className="medical_banner"></div>
+        </div>
+        <div className="col-xl-6 text-center login_bg screen w3-display-container">
+          <div className="w3-display-middle w-100 container">
+            <div className="row">
+              <div className="col-xl-8 offset-xl-2 col-lg-10 offset-lg-1">
+                <img
+                  src="logo_white.png"
+                  className="form-top-logo"
+                  alt="logo"
+                />
+                <h3 className="text-center text-white w3-bold">
+                  <b>DOCARE</b>
+                </h3>
 
-            <div className="text-center p-3">
-              <div className="px-1 pb-3 w3-block">
-                <b className="w3-left text-light">Email Address</b>
+                <b className="form-title">Forgot your password?</b>
+
+                <div className="text-center p-3">
+                  {error && <div className="alert alert-danger">{error}</div>}{" "}
+                  {/* Error message */}
+                  {success && (
+                    <div className="alert alert-success">{success}</div>
+                  )}{" "}
+                  {/* Success message */}
+                  <div className="px-1 pb-3 w3-block">
+                    <b className="w3-left text-light">Email Address</b>
+                  </div>
+                  <input
+                    type="email"
+                    className="form-control mt-3 text-medium"
+                    placeholder="Enter your email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)} // Update email state
+                  />
+                  <button
+                    className="btn btn-primary w3-block mt-4 mb-3 w3-round-large"
+                    type="button"
+                    onClick={handleReset} // Call handleReset on button click
+                  >
+                    RESET
+                  </button>
+                </div>
+
+                <div className="w3-block text-center navigator-footer">
+                  <b className="navigator" onClick={() => navigate("/")}>
+                    Go back to Login
+                  </b>
+                </div>
               </div>
-              <input
-                type="email"
-                className="form-control  mt-3  text-medium"
-                placeholder="Enter your Address"
-              />
-              <p className="fs-6 mt-4 text-light" style={{ fontSize: "20px" }}>
-                You will receive an email of your confirmation code
-              </p>
-
-              <button
-                className="btn btn-primary w3-block p-3 mt-4 mb-3 w3-round-large"
-                type="button"
-              >
-                RESET
-              </button>
-            </div>
-
-            <div className=" w3-block text-center ">
-              <b className="navigator" onClick={() => navigate("/")}>
-                Go back to Login
-              </b>
             </div>
           </div>
         </div>
--- src/components/sign-up.jsx
+++ src/components/sign-up.jsx
@@ -4,57 +4,70 @@ import { useNavigate } from "react-router-dom";
 const SignUp = () => {
   const navigate = useNavigate();
   return (
-    <div className="screen  login_bg bg-info w3-display-container ">
-      <div className="w3-display-middle w-100 container">
-        <div className="row">
-          <div className="col-lg-6 offset-lg-3 text-center">
-            <img src="logo_white.png" alt="logo" style={{ height: "80px" }} />
-            <h3 className="text-center text-white w3-bold">
-              <b>DOCARE</b>
-            </h3>
-            <b className="text-center text-white  mb-4 pt-5 pb-4 w3-block h5 bold">
-              Create your account
-            </b>
+    <div className=" bg-info container0 ">
+      <div className="row">
+        <div className="col-xl-6 p-0 d-none d-lg-block">
+          <div className="medical_banner"></div>
+        </div>
+        <div className="col-xl-6 text-center  login_bg screen w3-display-container">
+          <div className="w3-display-middle w-100 container">
+            <div className="row">
+              <div className=" col-xl-8  offset-xl-2  col-lg-10  offset-lg-1  ">
+                <img
+                  src="logo_white.png"
+                  className="form-top-logo"
+                  alt="logo"
+                />
+                <h3 className="text-center text-white w3-bold">
+                  <b>DOCARE</b>
+                </h3>
 
-            <div className="text-center p-3">
-              <div className="px-1 pb-3 w3-block">
-                <b className="w3-left text-light">Firstname</b>
-              </div>
-              <input
-                type="text"
-                className="form-control  mt-3  text-medium"
-                placeholder="Enter your Firstname"
-              />
-              <div className="px-1 pb-3 mt-3 w3-block">
-                <b className="w3-left text-light">Lastname</b>
-              </div>
-              <input
-                type="text"
-                className="form-control  mt-3  text-medium"
-                placeholder="Enter your Lastname"
-              />
-              <div className="px-1 pb-3 w3-block  mt-3 ">
-                <b className="w3-left text-light">Email Address</b>
-              </div>
-              <input
-                type="email"
-                className="form-control  mt-3  text-medium"
-                placeholder="Enter your Email"
-              />
+                <b className="form-title">Create your account</b>
 
-              <button
-                className="btn btn-primary w3-block p-3 mt-5 mb-3 w3-round-large"
-                type="button"
-              >
-                SIGN UP
-              </button>
-            </div>
+                <div className="text-center p-3">
+                  <div className="px-1 pb-3 w3-block">
+                    <b className="w3-left text-light">Firstname</b>
+                  </div>
+                  <input
+                    type="text"
+                    className="form-control  mt-3  text-medium"
+                    placeholder="Enter your Firstname"
+                  />
+                  <div className="px-1 pb-3 mt-3 w3-block">
+                    <b className="w3-left text-light">Lastname</b>
+                  </div>
+                  <input
+                    type="text"
+                    className="form-control  mt-3  text-medium"
+                    placeholder="Enter your Lastname"
+                  />
+                  <div className="px-1 pb-3 w3-block  mt-3 ">
+                    <b className="w3-left text-light">Email Address</b>
+                  </div>
+                  <input
+                    type="email"
+                    className="form-control  mt-3  text-medium"
+                    placeholder="Enter your Email"
+                  />
 
-            <div className=" w3-block text-center ">
-              Already have an account?
-              <b className="navigator px-2" onClick={() => navigate("/")}>
-                Login Here
-              </b>
+                  <button
+                    className="btn btn-primary w3-block  mt-5 mb-3 w3-round-large"
+                    type="button"
+                  >
+                    SIGN UP
+                  </button>
+                </div>
+
+                <div
+                  className=" w3-block text-center   navigator-footer"
+                  onClick={() => navigate("/")}
+                >
+                  <span className="navigator">
+                    Already have an account?
+                    <b className=" px-2">Login Here</b>
+                  </span>
+                </div>
+              </div>
             </div>
           </div>
         </div>
